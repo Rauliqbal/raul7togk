a09ff046dd891ecbe67e76b4a100a2f9e7b1cade
Rauliqbal <muhamadrauliqbal.13@gmail.com>
2024-11-21 17:23:44 +0700

    first commit

---
+++ tailwind.config.ts
@@ -0,0 +1,30 @@
+import type { Config } from "tailwindcss";
+
+const config: Config = {
+   content: [
+      "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
+      "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
+      "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+   ],
+   darkMode: ["class"],
+   theme: {
+      extend: {
+         colors: {
+            white: "#f1f1f1",
+         },
+         animation: {
+            scroll:
+               "scroll var(--animation-duration, 40s) var(--animation-direction, forwards) linear infinite",
+         },
+         keyframes: {
+            scroll: {
+               to: {
+                  transform: "translate(calc(-50% - 0.5rem))",
+               },
+            },
+         },
+      },
+   },
+   plugins: [],
+};
+export default config;
