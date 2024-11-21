2c92fdaebd24fdcae2898222a738134fe9efc7dd
Rauliqbal <muhamadrauliqbal.13@gmail.com>
2024-11-21 19:34:53 +0700

    feat: Section scroll text

--- src/components/hero.tsx
+++ src/components/hero.tsx
@@ -1,6 +1,5 @@
 "use client";
 
-import Image from "next/image";
 import { SlideIn, TextReveal, Transition } from "./ui";
 import { About } from "@/utils/interfaces";
 import Link from "next/link";
@@ -9,55 +8,59 @@ import { useState } from "react";
 import { PageLoad } from "./ui/page-load";
 
 export const Hero = ({ about }: { about: About }) => {
-  const [hideLoader, setHideLoader] = useState(true);
+   const [hideLoader, setHideLoader] = useState(true);
 
-  return (
-    <section className="h-dvh w-dvw overflow-hidden relative">
-      <span className="blob size-1/2 absolute top-20 left-0 blur-[100px]" />
-      {hideLoader ? (
-        <PageLoad hideLoader={hideLoader} setHideLoader={setHideLoader} />
-      ) : (
-        <div className="relative h-full w-full">
-          <div className="flex items-center justify-center flex-col h-full pb-10">
-            <Transition>
-              <Image
-                src={about.avatar.url}
-                width={200}
-                height={200}
-                alt={about.name}
-                className="rounded-full size-28 object-cover"
-              />
-            </Transition>
-            <div className="py-6 flex items-center flex-col">
-              <h2 className="md:text-7xl text-4xl font-bold overflow-hidden">
-                <SlideIn>Hello! I&apos;m {about.name}</SlideIn>
-              </h2>
-              <h1 className="md:text-7xl text-3xl overflow-hidden">
-                <SlideIn>{about.title}</SlideIn>
-              </h1>
+   return (
+      <section className="relative flex h-screen w-full flex-col items-center justify-center overflow-hidden ">
+         {hideLoader ? (
+            <PageLoad hideLoader={hideLoader} setHideLoader={setHideLoader} />
+         ) : (
+            <div className="px-4 md:px-8">
+               <div className="flex items-center justify-center flex-col h-[90vh]">
+                  <div className="py-6 flex items-center flex-col">
+                     <h1 className="md:text-9xl text-4xl font-bold overflow-hidden">
+                        <SlideIn>
+                           MUHAMAD <br />
+                           RAUL IQBAL
+                        </SlideIn>
+                     </h1>
+                     {/* <h1 className="md:text-7xl text-3xl overflow-hidden">
+                        <SlideIn></SlideIn>
+                     </h1> */}
+                  </div>
+                  <Transition viewport={{ once: true }} className="w-full">
+                     <p className="opacity-70 md:text-xl py-4 w-10/12 md:w-2/3 mx-auto flex flex-wrap justify-center gap-2">
+                        {"I am a developer and designer who has a passion for building responsive, cool looking, and easy to visit website applications."
+                           .split(" ")
+                           .map((word, index) => (
+                              <span key={index}>{word}</span>
+                           ))}
+                     </p>
+                  </Transition>
+                  <Transition viewport={{ once: true }}>
+                     <Link
+                        href={"#contact"}
+                        className="px-5 py-3 mt-4 rounded-full border border-white/50 flex items-center gap-4 group"
+                     >
+                        <TextReveal>Let&apos;s talk</TextReveal>
+                        <ArrowRight
+                           size={20}
+                           className="group-hover:rotate-90 transition-transform"
+                        />
+                     </Link>
+                  </Transition>
+               </div>
+               <div className="flex justify-between">
+                  <p className="text-lg font-medium">
+                     BASED IN BEKASI, INDONESIA
+                  </p>
+                  <p className="text-lg font-medium">
+                     FRONT-END DEVELOPER{" "}
+                     <span className="text-slate-400">+ UI DESIGNER</span>
+                  </p>
+               </div>
             </div>
-            <Transition viewport={{ once: true }} className="w-full">
-              <p className="opacity-70 md:text-xl py-4 w-10/12 md:w-2/3 mx-auto flex flex-wrap justify-center gap-2">
-                {about.subTitle.split(" ").map((word, index) => (
-                  <span key={index}>{word}</span>
-                ))}
-              </p>
-            </Transition>
-            <Transition viewport={{ once: true }}>
-              <Link
-                href={"#contact"}
-                className="px-5 py-3 mt-4 rounded-full border border-white/50 flex items-center gap-4 group"
-              >
-                <TextReveal>Let&apos;s talk</TextReveal>
-                <ArrowRight
-                  size={20}
-                  className="group-hover:rotate-90 transition-transform"
-                />
-              </Link>
-            </Transition>
-          </div>
-        </div>
-      )}
-    </section>
-  );
+         )}
+      </section>
+   );
 };
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -9,18 +9,27 @@ const config: Config = {
    darkMode: ["class"],
    theme: {
       extend: {
+         container: {
+            center: true,
+         },
          colors: {
             white: "#f1f1f1",
+            primary: "#525CEB",
          },
          animation: {
             scroll:
                "scroll var(--animation-duration, 40s) var(--animation-direction, forwards) linear infinite",
+            marquee: "marquee var(--duration) linear infinite",
          },
          keyframes: {
             scroll: {
                to: {
                   transform: "translate(calc(-50% - 0.5rem))",
                },
+            },
+            marquee: {
+               from: { transform: "translateX(0)" },
+               to: { transform: "translateX(calc(-100% - var(--gap)))" },
             },
          },
       },
