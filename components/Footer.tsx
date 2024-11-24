ec592ac749efe3aec7d1b8c172f7370557e974b4
Rauliqbal <muhamadrauliqbal.13@gmail.com>
2024-11-24 07:01:54 +0700

    feat:footer

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,9 +1,21 @@
+import Link from "next/link";
 import React from "react";
 
 export default function Footer() {
+   const date = new Date();
+
    return (
-      <footer>
-         <p className="text-[100%] font-bold font-instrument-sans">RAULIQBAL</p>
+      <footer className="px-4 md:px-8">
+         <p className="text-[14vw] leading-none mx-auto text-center font-bold font-instrument-sans">
+            RAULIQBAL
+         </p>
+         <div className="flex items-center justify-between py-8">
+            <div>
+               <p>&copy; {`${date.getFullYear()} RAULIQBAL`}</p>
+            </div>
+
+            <Link href={"/"}>GO BACK TO TOP</Link>
+         </div>
       </footer>
    );
 }
